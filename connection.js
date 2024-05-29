c1b98e06386412f58f4c6683aa64818effa15fae
Matheus suehtam <162381453+DNSTHELIMITLESS@users.noreply.github.com>
2024-05-28 22:16:04 -0300

    Create connection.js

---
+++ connection.js
@@ -0,0 +1,25 @@
+import pkg from 'mongoose';
+const { connect, connection, disconnect } = pkg;
+
+const uri = process.env.MONGO_DB_URL;
+
+const clientOptions = {
+  serverApi: {
+    version: '1',
+    strict: true,
+    deprecationErrors: true
+  }
+};
+
+async function run() {
+  try {
+
+    await connect(uri, clientOptions);
+    await connection.db.admin().command({ ping: 1 });
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Error: ", error);
+
+  }
+}
+run().catch((error) => console.log(error));
